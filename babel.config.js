db871af8a2bc5c901d28bedb9f7a4956ed817ada
Nata Souza Santos <natasouza.santos50@gmail.com>
2021-03-16 22:18:57 -0300

    configure project

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@contexts': './src/contexts',
+          '@interfaces': './src/interfaces',
+          '@services': './src/services',
+          '@constants': './src/constants',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
